70e25927d547a0792de00a1c5a877131d6e0ee1d
mrijk <mrijk>
2005-05-13 20:47:29 +0000

    First steps towards 0.8

--- lib/Drawable.cs
+++ lib/Drawable.cs
@@ -197,7 +197,7 @@ namespace Gimp
                        int offset_x, int offset_y)
       {
       if (!gimp_drawable_offset(_ID, wrap_around, fill_type, offset_x, 
-                                offset_y))
+				offset_y))
         {
         throw new Exception();
         }
@@ -233,14 +233,6 @@ namespace Gimp
         }
       }
 
-    public void Desaturate()
-      {
-      if (!gimp_desaturate(_ID))
-        {
-        throw new Exception();
-        }
-      }
-
     public void FuzzySelect(double x, double y, int threshold, 
                             ChannelOps operation, bool antialias,
                             bool feather, double feather_radius,
@@ -253,6 +245,150 @@ namespace Gimp
         throw new Exception();
         }
       }
+
+    // GimpColor
+
+    public void BrightnessContrast(int brightness, int contrast)
+      {
+      if (!gimp_brightness_contrast(_ID, brightness, contrast))
+        {
+        throw new Exception();
+        }
+      }
+
+    public void Levels(HistogramChannel channel,
+                       int low_input, int high_input,
+                       double gamma,
+                       int low_output,
+                       int high_output)
+      {
+      if (!gimp_levels(_ID, channel, low_input, high_input,
+                       gamma, low_output, high_output))
+        {
+        throw new Exception();
+        }
+      }
+
+    public void LevelsStretch()
+      {
+      if (!gimp_levels_stretch(_ID))
+        {
+        throw new Exception();
+        }
+      }
+
+    public void Posterize(int levels)
+      {
+      if (!gimp_posterize(_ID, levels))
+        {
+        throw new Exception();
+        }
+      }
+
+    public void Desaturate()
+      {
+      if (!gimp_desaturate(_ID))
+        {
+        throw new Exception();
+        }
+      }
+
+    public void Desaturate(DesaturateMode desaturate_mode)
+      {
+      if (!gimp_desaturate_full(_ID, desaturate_mode))
+        {
+        throw new Exception();
+        }
+      }
+
+    public void Equalizee(bool mask_only)
+      {
+      if (!gimp_equalize(_ID, mask_only))
+        {
+        throw new Exception();
+        }
+      }
+
+    public void Invert()
+      {
+      if (!gimp_invert(_ID))
+        {
+        throw new Exception();
+        }
+      }
+
+    public void CurvesSpline(HistogramChannel channel)
+      {
+      throw new Exception("Fixme: CurvesSpline not implemented yet!");
+      }
+
+    public void CurvesExplicit(HistogramChannel channel)
+      {
+      throw new Exception("Fixme: CurvesExplicit not implemented yet!");
+      }
+
+    public void ColorBalance(TransferMode transfer_mode,
+                             bool preserve_lum,
+                             double cyan_red,
+                             double magenta_green,
+                             double yellow_blue)
+      {
+      if (!gimp_color_balance(_ID, transfer_mode, preserve_lum,
+                              cyan_red, magenta_green, yellow_blue))
+        {
+        throw new Exception();
+        }
+      }
+
+    public void Colorize(double hue, double saturation, double lightness)
+      {
+      if (!gimp_colorize(_ID, hue, saturation, lightness))
+        {
+        throw new Exception();
+        }
+      }
+
+    public void Histogram(HistogramChannel channel,
+                          int start_range,
+                          int end_range,
+                          out double mean,
+                          out double std_dev,
+                          out double median,
+                          out double pixels,
+                          out double count,
+                          out double percentile)
+      {
+      if (!gimp_histogram(_ID, channel, start_range, end_range,
+                          out mean, out std_dev, out median, out pixels, 
+                          out count, out percentile))
+        {
+        throw new Exception();
+        }
+      }
+
+    public void HueSaturation(HueRange hue_range,
+                              double hue_offset,
+                              double lightness,
+                              double saturation)
+      {
+      if (!gimp_hue_saturation(_ID, hue_range, hue_offset,
+                               lightness, saturation))
+        {
+        throw new Exception();
+        }
+      }
+
+    public void Threshold(int low_threshold,
+                          int high_threshold)
+      {
+      if (!gimp_threshold(_ID, low_threshold,
+                          high_threshold))
+        {
+        throw new Exception();
+        }
+      }
+
+    // Misc routines
 
     public Int32 ID
       {
@@ -375,8 +511,75 @@ namespace Gimp
                                                          int size,
                                                          object data);
 
+
+    [DllImport("libgimp-2.0-0.dll")]
+    static extern bool gimp_brightness_contrast (Int32 drawable_ID,
+                                                 int brightness, int contrast);
+    [DllImport("libgimp-2.0-0.dll")]
+    static extern bool gimp_levels (Int32 drawable_ID,
+                                    HistogramChannel channel,
+                                    int low_input,
+                                    int high_input,
+                                    double gamma,
+                                    int low_output,
+                                    int high_output);
+    [DllImport("libgimp-2.0-0.dll")]
+    static extern bool gimp_levels_stretch (Int32 drawable_ID);
+    [DllImport("libgimp-2.0-0.dll")]
+    static extern bool gimp_posterize (Int32 drawable_ID, int levels);
     [DllImport("libgimp-2.0-0.dll")]
     static extern bool gimp_desaturate (Int32 drawable_ID);
+    [DllImport("libgimp-2.0-0.dll")]
+    static extern bool gimp_desaturate_full (Int32 drawable_ID,
+                                             DesaturateMode desaturate_mode);
+    [DllImport("libgimp-2.0-0.dll")]
+    static extern bool gimp_equalize (Int32 drawable_ID,
+                                      bool mask_only);
+    [DllImport("libgimp-2.0-0.dll")]
+    static extern bool gimp_invert (Int32 drawable_ID);
+    [DllImport("libgimp-2.0-0.dll")]
+    static extern bool gimp_curves_spline (Int32 drawable_ID,
+                                           HistogramChannel channel,
+                                           int num_points,
+                                           byte[] control_pts);
+    [DllImport("libgimp-2.0-0.dll")]
+    static extern bool gimp_curves_spline_explicit (Int32 drawable_ID,
+                                                    HistogramChannel channel,
+                                                    int num_points,
+                                                    byte[] control_pts);
+    [DllImport("libgimp-2.0-0.dll")]
+    static extern bool gimp_color_balance (Int32 drawable_ID,
+                                           TransferMode transfer_mode,
+                                           bool preserve_lum,
+                                           double cyan_red,
+                                           double magenta_green,
+                                           double yellow_blue);
+    [DllImport("libgimp-2.0-0.dll")]
+    static extern bool gimp_colorize (Int32 drawable_ID,
+                                      double hue,
+                                      double saturation,
+                                      double lightness);
+    [DllImport("libgimp-2.0-0.dll")]
+    static extern bool gimp_histogram (Int32 drawable_ID,
+                                       HistogramChannel channel,
+                                       int start_range,
+                                       int end_range,
+                                       out double mean,
+                                       out double std_dev,
+                                       out double median,
+                                       out double pixels,
+                                       out double count,
+                                       out double percentile);
+    [DllImport("libgimp-2.0-0.dll")]
+    static extern bool gimp_hue_saturation (Int32 drawable_ID,
+                                            HueRange hue_range,
+                                            double hue_offset,
+                                            double lightness,
+                                            double saturation);
+    [DllImport("libgimp-2.0-0.dll")]
+    static extern bool gimp_threshold (Int32 drawable_ID,
+                                       int low_threshold,
+                                       int high_threshold);
 
     [DllImport("libgimp-2.0-0.dll")]
     static extern bool gimp_fuzzy_select (Int32 drawable_ID,
