1c4fd342e868317a06b276a26c0056f54760e49b
mrijk <mrijk>
2006-06-25 14:45:17 +0000

    Forgotten ;)

---
+++ plug-ins/PhotoshopActions/TilesEvent.cs
@@ -0,0 +1,58 @@
+// The PhotoshopActions plug-in
+// Copyright (C) 2006 Maurits Rijk
+//
+// TilesEvent.cs
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+
+using System;
+
+using Gtk;
+
+namespace Gimp.PhotoshopActions
+{
+  public class TilesEvent : ActionEvent
+  {
+    [Parameter("TlNm")]
+    int _numberOfTiles;
+    [Parameter("TlOf")]
+    int _maximumOffset;
+    [Parameter("FlCl")]
+    EnumParameter _fillColor;
+
+    protected override void FillParameters(TreeStore store, TreeIter iter)
+    {
+      store.AppendValues(iter, "Number Of Tiles: " + _numberOfTiles);
+      store.AppendValues(iter, "Maximum Offset: " + _maximumOffset);
+      store.AppendValues(iter, "Fill Empty With: " + _fillColor.Value);
+    }
+
+    override public bool Execute()
+    {
+      // TODO: check _fillColor type
+
+      int tileSize = (int) Math.Sqrt(ActiveDrawable.Width * 
+				     ActiveDrawable.Height /
+				     _numberOfTiles);
+
+      RunProcedure("plug_in_papertile", tileSize, 
+		   _maximumOffset * 100.0 / tileSize, 0, 1, 1,
+		   4, 0, 0);
+
+      return true;
+    }
+  }
+}
