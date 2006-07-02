4332cb35b4ac698eeee26c8ed4c426fc534e1a0e
neppanepenthes <neppanepenthes>
2006-07-02 16:32:23 +0000

    [Max] Fixed bug #1514493

--- lib/Gimp.cs
+++ lib/Gimp.cs
@@ -223,7 +223,7 @@ namespace Gimp
     [DllImport("libgimp-2.0-0.dll")]
     static extern Int32 gimp_default_display();
 
-    [DllImport("libgimp-2.0-0.dll")]
+    [DllImport("libgimpbase-2.0-0.dll")]
     static extern IntPtr gimp_directory();
 
     [DllImport("libgimp-2.0-0.dll")]
