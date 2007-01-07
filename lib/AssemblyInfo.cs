9725a4ce8737d6cab16848318f5eb7e5810db423
mrijk <mrijk>
2007-01-07 21:06:57 +0000

    2007-01-07  Maurits  <m.rijk@chello.nl>
    	* README: minor update
    
    	* lib/AssemblyInfo.cs
    	* INSTALL: bumped version number to 0.12
    
    	* configure.in: bumped version number to 0.12 and depend on Mono 1.2.x
    
    2007-01-06  Maurits  <m.rijk@chello.nl>
    
    	* Made 0.11 release
    
    	* NEWS: update

--- lib/AssemblyInfo.cs
+++ lib/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 // GIMP# - A C# wrapper around the GIMP Library
-// Copyright (C) 2004-2006 Maurits Rijk
+// Copyright (C) 2004-2007 Maurits Rijk
 //
 // AssemblyInfo.cs
 //
@@ -27,8 +27,8 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Maurits Rijk")]
 [assembly: AssemblyProduct("GIMP#")]
-[assembly: AssemblyCopyright("©2004-2006 All Rights Reserved")]
+[assembly: AssemblyCopyright("©2004-2007 All Rights Reserved")]
 [assembly: AssemblyTrademark("")]
 #endregion
 
-[assembly: AssemblyVersion("0.11.0")]
+[assembly: AssemblyVersion("0.12.0")]
