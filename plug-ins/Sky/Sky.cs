60e9ad89d5be1d82579fd720b4f3768e2b8c7284
mrijk <mrijk>
2007-12-07 22:43:50 +0000

    2007-12-07  Maurits  <maurits.rijk@gmail.com>
    	* plug-ins/Makefile.am
    	* plug-ins/Sky/*
    	* configure.in: add initial Sky plug-in code

---
+++ plug-ins/Sky/Sky.cs
@@ -0,0 +1,107 @@
+// The Sky plug-in
+// Copyright (C) 2004-2007 Maurits Rijk
+//
+// Sky.cs
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+
+using System;
+using System.Collections.Generic;
+
+using Gtk;
+
+namespace Gimp.Sky
+{
+  class Sky : Plugin
+  {
+    DrawablePreview _preview;
+
+    static void Main(string[] args)
+    {
+      new Sky(args);
+    }
+
+    Sky(string[] args) : base(args, "Sky")
+    {
+    }
+
+    override protected IEnumerable<Procedure> ListProcedures()
+    {
+      yield return new Procedure("plug_in_sky",
+				 _("Sky"),
+				 _("Sky"),
+				 "Maurits Rijk",
+				 "(C) Maurits Rijk",
+				 "2007",
+				 _("Sky..."),
+				 "RGB*")
+	{
+	  MenuPath = "<Image>/Media/",
+	  IconFile = "Sky.png"
+	};
+    }
+
+    override protected GimpDialog CreateDialog()
+    {
+      gimp_ui_init("Sky", true);
+
+      GimpDialog dialog = DialogNew("Sky", "Sky", IntPtr.Zero, 0,
+				    Gimp.StandardHelpFunc, "Sky");
+
+      VBox vbox = new VBox(false, 12) {BorderWidth = 12};
+      dialog.VBox.PackStart(vbox, true, true, 0);
+
+      _preview = new DrawablePreview(_drawable, false);
+      _preview.Invalidated += UpdatePreview;
+      vbox.PackStart(_preview, true, true, 0);
+
+      return dialog;
+    }
+
+    void UpdatePreview(object sender, EventArgs e)
+    {
+      Rectangle rectangle = _preview.Bounds;
+
+      int rowStride = rectangle.Width * 3;
+      byte[] buffer = new byte[rectangle.Area * 3];	// Fix me!
+
+      PixelRgn srcPR = new PixelRgn(_drawable, rectangle, false, false);
+
+      RegionIterator iterator = new RegionIterator(srcPR);
+      iterator.ForEach(src =>
+	{
+	  int x = src.X;
+	  int y = src.Y;
+	  Pixel pixel = DoSky(src);
+	  
+	  int index = (y - rectangle.Y1) * rowStride + (x - rectangle.X1) * 3;
+	  pixel.CopyTo(buffer, index);
+	});
+      _preview.DrawBuffer(buffer, rowStride);
+    }
+
+    override protected void Render(Drawable drawable)
+    {
+      Display.DisplaysFlush();
+    }
+
+    Pixel DoSky(Pixel pixel)
+    {
+      pixel.Fill(val => (val > 127) ? 255 : 0);
+      return pixel;
+    }
+  }
+}
