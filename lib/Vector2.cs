7fd454f26ee184de2ab6360554ef4477a902938b
mrijk <mrijk>
2008-01-03 21:11:32 +0000

    2007-12-28  Maurits  <maurits.rijk@gmail.com>
    	* lib/Vector2.cs: more coverage
    
    	* plug-ins/UnitTest/TestVector2.cs: more tests

--- lib/Vector2.cs
+++ lib/Vector2.cs
@@ -19,28 +19,128 @@
 // Boston, MA 02111-1307, USA.
 //
 
-using System;
 using System.Runtime.InteropServices;
 
 namespace Gimp
 {
   public class Vector2
   {
-    readonly GimpVector2 _vector;
+    GimpVector2 _vector;
 
     public Vector2(double x, double y)
     {
       _vector = gimp_vector2_new(x, y);
     }
 
+    public Vector2() : this(0.0, 0.0)
+    {
+    }
+
+    Vector2(Vector2 vector) : this(vector.X, vector.Y)
+    {
+    }
+
+    public void Set(double x, double y)
+    {
+      gimp_vector2_set(ref _vector, x, y);
+    }
+
+    public double Length
+    {
+      get {return gimp_vector2_length(ref _vector);}
+    }
+
     public double X
     {
       get {return _vector.x;}
+      set {_vector.x = value;}
     }
 
     public double Y
     {
       get {return _vector.y;}
+      set {_vector.y = value;}
+    }
+
+    public override bool Equals(object o)
+    {
+      if (o is Vector2)
+	{
+	  Vector2 vector = o as Vector2;
+	  return vector.X == X && vector.Y == Y;
+	}
+      return false;
+    }
+
+    public override int GetHashCode()
+    {
+      return X.GetHashCode() + Y.GetHashCode();
+    }
+
+    public void Mul(double factor)
+    {
+      gimp_vector2_mul(ref _vector, factor);
+    }
+
+    public void Normalize()
+    {
+      gimp_vector2_normalize(ref _vector);
+    }
+
+    public void Neg()
+    {
+      gimp_vector2_neg(ref _vector);
+    }
+
+    public Vector2 Add(Vector2 vector)
+    {
+      gimp_vector2_add(ref _vector, ref _vector, ref vector._vector);
+      return this;
+    }
+
+    public Vector2 Sub(Vector2 vector)
+    {
+      gimp_vector2_sub(ref _vector, ref _vector, ref vector._vector);
+      return this;
+    }
+
+    public static Vector2 operator + (Vector2 v1, Vector2 v2)
+    {
+      return (new Vector2(v1)).Add(v2);
+    }
+
+    public static Vector2 operator - (Vector2 v1, Vector2 v2)
+    {
+      return (new Vector2(v1)).Sub(v2);
+    }
+
+    public static Vector2 operator - (Vector2 vector)
+    {
+      Vector2 v = new Vector2(vector);
+      v.Neg();
+      return v;
+    }
+
+    public static Vector2 operator * (Vector2 vector, double factor)
+    {
+      Vector2 v = new Vector2(vector);
+      v.Mul(factor);
+      return v;
+    }
+
+    public double InnerProduct(Vector2 vector)
+    {
+      return gimp_vector2_inner_product(ref _vector, ref vector._vector);
+    }
+
+    public double CrossProduct(Vector2 vector)
+    {
+      return gimp_vector2_cross_product(ref _vector, ref vector._vector);
+    }
+
+    public void Rotate(double alpha, double beta, double gamma)
+    {
+      gimp_vector2_rotate(ref _vector, alpha, beta, gamma);
     }
 
     public override string ToString()
@@ -51,6 +151,38 @@ namespace Gimp
     [DllImport("libgimpmath-2.0-0.dll")]
     static extern GimpVector2 gimp_vector2_new(double x,
 					       double y);
+    [DllImport("libgimpmath-2.0-0.dll")]
+    static extern void gimp_vector2_set(ref GimpVector2 vector,
+					double x,
+					double y);
+    [DllImport("libgimpmath-2.0-0.dll")]
+    static extern double gimp_vector2_length(ref GimpVector2 vector);
+    [DllImport("libgimpmath-2.0-0.dll")]
+    static extern void gimp_vector2_mul(ref GimpVector2 vector,
+					double factor);
+    [DllImport("libgimpmath-2.0-0.dll")]
+    static extern void gimp_vector2_normalize(ref GimpVector2 vector);
+    [DllImport("libgimpmath-2.0-0.dll")]
+    static extern void gimp_vector2_neg(ref GimpVector2 vector);
+    [DllImport("libgimpmath-2.0-0.dll")]
+    static extern void gimp_vector2_add(ref GimpVector2 result,
+					ref GimpVector2 vector1,
+					ref GimpVector2 vector2);
+    [DllImport("libgimpmath-2.0-0.dll")]
+    static extern void gimp_vector2_sub(ref GimpVector2 result,
+					ref GimpVector2 vector1,
+					ref GimpVector2 vector2);
+    [DllImport("libgimpmath-2.0-0.dll")]
+    static extern double gimp_vector2_inner_product(ref GimpVector2 vector1,
+						    ref GimpVector2 vector2);
+    [DllImport("libgimpmath-2.0-0.dll")]
+    static extern double gimp_vector2_cross_product(ref GimpVector2 vector1,
+						    ref GimpVector2 vector2);
+    [DllImport("libgimpmath-2.0-0.dll")]
+    static extern void gimp_vector2_rotate(ref GimpVector2 vector,
+					   double alpha,
+					   double beta,
+					   double gamma);
   }
 
   [StructLayout(LayoutKind.Sequential)]
--- plug-ins/UnitTest/TestVector2.cs
+++ plug-ins/UnitTest/TestVector2.cs
@@ -35,5 +35,117 @@ namespace Gimp
       Assert.AreEqual(13, vector.X);
       Assert.AreEqual(14, vector.Y);
     }
+
+    [Test]
+    public void DefaultConstructor()
+    {
+      Vector2 vector = new Vector2();
+      Assert.AreEqual(0.0, vector.X);
+      Assert.AreEqual(0.0, vector.Y);
+    }
+
+    [Test]
+    public void Set()
+    {
+      Vector2 vector = new Vector2();
+      vector.Set(13, 14);
+      Assert.AreEqual(13, vector.X);
+      Assert.AreEqual(14, vector.Y);
+    }
+
+    [Test]
+    public void Length()
+    {
+      Vector2 vector = new Vector2();
+      Assert.AreEqual(0.0, vector.Length);
+
+      vector = new Vector2(3, 4);
+      Assert.AreEqual(5.0, vector.Length);
+    }
+
+    [Test]
+    public void GetSetXYZ()
+    {
+      Vector2 vector = new Vector2();
+      vector.X = 13;
+      vector.Y = 14;
+      Assert.AreEqual(13, vector.X);
+      Assert.AreEqual(14, vector.Y);
+    }
+
+    [Test]
+    public void Mul()
+    {
+      Vector2 vector = new Vector2(1, 2);
+      vector.Mul(2);
+      Assert.AreEqual(new Vector2(2, 4), vector);
+    }
+
+    [Test]
+    public void Normalize()
+    {
+      Vector2 vector = new Vector2(1, 2);
+      vector.Normalize();
+      Assert.AreEqual(1, vector.Length);
+    }
+
+    [Test]
+    public void Neg()
+    {
+      Vector2 vector = new Vector2(1, 2);
+      vector.Neg();
+      Assert.AreEqual(new Vector2(-1, -2), vector);
+    }
+
+    [Test]
+    public void Add()
+    {
+      Vector2 v1 = new Vector2(1, 2);
+      Vector2 v2 = new Vector2(1, 1);
+      v1.Add(v2);
+      Assert.AreEqual(new Vector2(2, 3), v1);
+    }
+
+    [Test]
+    public void Sub()
+    {
+      Vector2 v1 = new Vector2(1, 2);
+      Vector2 v2 = new Vector2(1, 1);
+      v1.Sub(v2);
+      Assert.AreEqual(new Vector2(0, 1), v1);
+    }
+
+    [Test]
+    public void PlusOperator()
+    {
+      Vector2 v1 = new Vector2(1, 2);
+      Vector2 v2 = new Vector2(1, 1);
+      Vector2 v = v1 + v2;
+      Assert.AreEqual(new Vector2(2, 3), v);
+    }
+
+    [Test]
+    public void MinusOperator()
+    {
+      Vector2 v1 = new Vector2(1, 2);
+      Vector2 v2 = new Vector2(1, 1);
+      Vector2 v = v1 - v2;
+      Assert.AreEqual(new Vector2(0, 1), v);
+    }
+
+    [Test]
+    public void MultiplyOperator()
+    {
+      Vector2 v1 = new Vector2(1, 2);
+      Vector2 v = v1 * 3;
+      Assert.AreEqual(new Vector2(3, 6), v);
+    }    
+
+    [Test]
+    public void InnerProduct()
+    {
+      Vector2 v1 = new Vector2(1, 2);
+      Assert.AreEqual(1 * 1 + 2 * 2, v1.InnerProduct(v1));
+    }
   }
 }
